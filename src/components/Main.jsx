b7f61bbb4afab998290af1f668e1926154f5150d
JahmirKS <98054292+JahmirKS@users.noreply.github.com>
2023-06-05 02:49:25 -0500

    About page mobile-view done

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import Button from './Global_components/Button'
 import Specials_Container from './Global_components/Specials_Container'
 import Testimonials_Container from './Global_components/Testimonials_Container'
+import about_img from '../assets/MA_A.jpg'
 
 export class Main extends Component {
   render() {
@@ -34,6 +35,33 @@ export class Main extends Component {
           </h2>
 
           <Testimonials_Container />
+        </section>
+
+        <section className="flex flex-col items-start justify-center px-4  py-4 ">
+          <h2 className="text-title text-Green font-Markazi-medium  font-medium flex flex-col gap-1">
+            <span className=" h-16">Little Lemon </span>
+            <span className=" h-10 text-Dark_Orange text-sub_title font-normal">
+              Chicago
+            </span>
+            <img
+              src={about_img}
+              alt=""
+              width="272"
+              className=" rounded-md shadow-md shadow-Black my-3 "
+            />
+          </h2>
+          <details className="text-Black text-medium font-Karla-medium font-medium w-60">
+            <summary>About us</summary>
+            <p>
+              Little Lemon is owned by two Italian brothers, Mario and Adrian,
+              who moved to the United States to pursue their shared dream of
+              owning a restaurant. To craft the menu, Mario relies on family
+              recipes and his experience as a chef in Italy. Adrian does all the
+              marketing for the restaurant and led the effort to expand the menu
+              beyond classic Italian to incorporate additional cuisines from the
+              Mediterranean region.
+            </p>
+          </details>
         </section>
       </main>
     )
