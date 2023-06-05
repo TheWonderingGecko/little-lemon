f1d2c1a2340ae6e87089e55a8bc7d5819a5c32b9
JahmirKS <98054292+JahmirKS@users.noreply.github.com>
2023-06-05 07:39:45 -0500

    about medium displays

--- src/components/Global_components/Testimonials_Container.jsx
+++ src/components/Global_components/Testimonials_Container.jsx
@@ -2,7 +2,7 @@ import Testimonials_Card from './Testimonials_Card'
 
 const Testimonials_Container = () => {
   return (
-    <div className="flex overflow-x-auto w-full gap-4 p-4  ">
+    <div className="flex overflow-x-auto w-full gap-4 p-4 md:px-0 ">
       <Testimonials_Card />
       <Testimonials_Card />
       <Testimonials_Card />
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -4,6 +4,7 @@ import Button from './Global_components/Button'
 import Specials_Container from './Global_components/Specials_Container'
 import Testimonials_Container from './Global_components/Testimonials_Container'
 import about_img from '../assets/MA_A.jpg'
+import restaurant from '../assets/restaurant.jpg'
 import Hero_img from '../assets/restauranfood.jpg'
 
 export class Main extends Component {
@@ -33,7 +34,7 @@ export class Main extends Component {
           <Specials_Container />
         </section>
 
-        <section className="flex flex-col items-center justify-center bg-Green py-4  ">
+        <section className="flex flex-col items-center justify-center bg-Green py-4 md:px-60 md:py-20  ">
           <h2 className="text-4xl text-Yellow font-Markazi-medium font-medium  w-3/4 text-center">
             Testimonials
           </h2>
@@ -42,7 +43,7 @@ export class Main extends Component {
         </section>
 
         <section
-          className="flex flex-col items-center justify-center px-4  py-4 "
+          className=" md:hidden flex flex-col items-center justify-center px-4  py-4 "
           id="about"
         >
           <h2 className="text-title text-Green font-Markazi-medium  font-medium flex flex-col gap-1">
@@ -50,13 +51,13 @@ export class Main extends Component {
             <span className=" h-10 text-Dark_Orange text-sub_title font-normal">
               Chicago
             </span>
-            <img
-              src={about_img}
-              alt=""
-              width="272"
-              className=" rounded-md shadow-md shadow-Black my-3 "
-            />
           </h2>
+          <img
+            src={about_img}
+            alt=""
+            width="272"
+            className=" rounded-md shadow-md shadow-Black my-3 "
+          />
           <details className="text-Black text-medium font-Karla-bold font-bold ">
             <summary>About us</summary>
             <p>
@@ -69,6 +70,44 @@ export class Main extends Component {
               Mediterranean region.
             </p>
           </details>
+        </section>
+
+        <section
+          className=" hidden w-full  md:flex  items-center justify-between px-60  py-20  h-full md:relative gap-8"
+          id="about"
+        >
+          <div className="w-[370px] basis-1/2">
+            <h2 className="text-title text-Green font-Markazi-medium  font-medium flex flex-col gap-1">
+              <span className=" h-16">Little Lemon </span>
+              <span className=" h-10 text-Dark_Orange text-sub_title font-normal">
+                Chicago
+              </span>
+            </h2>
+
+            <p className="text-Black text-medium font-Karla-bold font-bold">
+              Little Lemon is owned by two Italian brothers, Mario and Adrian,
+              who moved to the United States to pursue their shared dream of
+              owning a restaurant. To craft the menu, Mario relies on family
+              recipes and his experience as a chef in Italy. Adrian does all the
+              marketing for the restaurant and led the effort to expand the menu
+              beyond classic Italian to incorporate additional cuisines from the
+              Mediterranean region.
+            </p>
+          </div>
+
+          <div className="flex flex-col basis-1/2 ">
+            <img
+              className=" absolute z-10 top-[5%] right-[18%] object-cover w-[276px] h-[338px] shadow-md  border  border-black "
+              src={about_img}
+              alt="Image 1"
+            />
+
+            <img
+              className=" absolute  bottom-[5%] object-cover w-[276px] h-[338px] shadow-md  border  border-black"
+              src={restaurant}
+              alt="Image 2"
+            />
+          </div>
         </section>
       </main>
     )
